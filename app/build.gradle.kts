e1d870144c3bb8a532451175db08f9e7dc16190d
dsh-android <dsh-android@users.noreply.github.com>
2026-08-17 21:23:03 +0800

    Set versionName to 0.1.0 for the first release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 28
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0.0"
+        versionName = "0.1.0"
 
         vectorDrawables { useSupportLibrary = true }
     }
